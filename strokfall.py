25ef7e3b63639cf5f6f8eba6cbee2d73deb35359
Niklas Kjenstadbakk Brede <114137789+nikkbred@users.noreply.github.com>
2023-08-19 16:18:57 +0200

    Update strokfall.py

--- strokfall.py
+++ strokfall.py
@@ -124,7 +124,7 @@ def trigger():
 
         with kolll2:
             # S-pol
-            figs = plt.figure(figsize=(20, 9))
+            figs = plt.figure(figsize=(12, 12))
 
             axs = figs.add_subplot(projection='stereonet')
             axs.pole(strikes, dips, c='k', label='Polene')
